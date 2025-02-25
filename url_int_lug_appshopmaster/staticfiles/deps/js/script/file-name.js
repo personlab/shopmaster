3ee7a37c19f2d0925804b3306a2552d79561269b
Yuriy Baragin <yubarmalei@gmail.com>
2025-02-25 11:33:35 +0300

    version.3.14.1

--- url_int_lug_appshopmaster/staticfiles/deps/js/script/file-name.js
+++
@@ -1,9 +0,0 @@
-function updateFileName() {
-	var input = document.getElementById('id_image');
-	var fileNameDisplay = document.getElementById('file-name');
-	if (input.files.length > 0) {
-		fileNameDisplay.textContent = input.files[0].name; // Отображаем имя файла вместо текста
-	} else {
-		fileNameDisplay.textContent = 'Выберите файл'; // Возвращаем исходный текст, если файл не выбран
-	}
-}
